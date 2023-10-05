e50b6fd027bce1f7a5e239ead322c0c824ce72ea
zso-win <zso-win@outlook.com>
2023-10-05 23:08:26 +0200

    Add display logic for images, text depending on question status

--- src/views/QuestionView.vue
+++ src/views/QuestionView.vue
@@ -1,36 +1,50 @@
 <script setup lang="ts">
 
-import ZsQuestion1 from '@/components/ZsQuestion1.vue';
+import ThePanel from '@/components/ThePanel.vue';
 import { useAnswerStore } from '@/stores/answers';
 import { useQuestionStore } from '@/stores/questions';
 import { resources } from '@/views/resources'
 
 const props = defineProps({
   id: String,
-  org: String
+  org: String,
+  name: String
 })
 
-let key = `${props.org}_${props.id}`
+let msg = "Dieser QR Code is für eine andere Frage. Suche weiter."
+let img = "/assets/ambulance.jpg"
+
+const questionKey = `${props.org}${props.id}`
+const key = `${questionKey}_${props.name}`
 
 const answers = useAnswerStore()
 const questions = useQuestionStore()
 
 const res = resources[key]
 
-// TODO add update logic for questions
-if (props.id == "q") {
-  questions.update({})
+if (!res) {
+  msg = "Oooops. Etwas ist schiefgelaufen. Versuche einen anderen QR Code."
 }
 
-// TODO add update logic for answers
-if (res["rightAnswer"]) {
-  answers.update({})
+if (props.name == "q") {
+  questions.activateQuestion(questionKey);
+  msg = res.msg
+  img = res.img
+}
+
+if (res && questions.isActive(questionKey)) {
+  msg = res.msg
+  img = res.img
+
+  if (res["rightAnswer"]) {
+    answers.rightAnswer(questionKey)
+  }
 }
 
 </script>
 
 <template>
   <main>
-    <ZsQuestion1 :msg="res.msg" :img="res.img" />
+    <ThePanel :msg="msg" :img="img" />
   </main>
 </template>

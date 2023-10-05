7d15ab8ef35782e9e05f9c0667f70abcd178fa72
zso-win <zso-win@outlook.com>
2023-10-05 23:07:17 +0200

    Rename main component

---
+++ src/components/ThePanel.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import { NSpace, NImage } from 'naive-ui'
+
+const props = defineProps({
+    msg: String,
+    img: String
+})
+
+</script>
+
+<template>
+    <div>
+        <n-space justify="center">
+            <n-image width="350" :src="img" object-fit="scale-down" height="200" />
+        </n-space>
+
+        <p>{{ msg }}</p>
+
+    </div>
+</template>
+
+<style></style>
